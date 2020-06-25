9461e0acd98befc115d1ce7f65e30dcf3e8281cd
vMIAMI <1411079322@qq.com>
2020-06-25 19:24:45 +0800

    eureka集群/子节点间调用loadbalance

---
+++ cloud-provider-payment8002/src/main/java/com/vime/service/impl/PaymentServiceImpl.java
@@ -0,0 +1,24 @@
+package com.vime.service.impl;
+
+import com.vime.dao.PaymentDao;
+import com.vime.entites.Payment;
+import com.vime.service.PaymentService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+public class PaymentServiceImpl implements PaymentService {
+    @Resource
+    private PaymentDao paymentDao;
+
+    @Override
+    public int create(Payment payment) {
+        return paymentDao.create(payment);
+    }
+
+    @Override
+    public Payment getPaymentById(Long id) {
+        return paymentDao.getPaymentById(id);
+    }
+}
