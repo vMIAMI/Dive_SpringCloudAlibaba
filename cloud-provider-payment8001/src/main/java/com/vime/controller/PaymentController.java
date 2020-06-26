933a1730cb7e5173d991b04c07874d5514390145
vMIAMI <1411079322@qq.com>
2020-06-26 13:32:19 +0800

    服务发现 discovery

--- cloud-provider-payment8001/src/main/java/com/vime/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/vime/controller/PaymentController.java
@@ -5,9 +5,12 @@ import com.vime.entites.Payment;
 import com.vime.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -18,6 +21,9 @@ public class PaymentController {
 
     @Resource
     private PaymentService paymentService;
+
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     @RequestMapping(value = "/payment/tt")
     public String tt() {
@@ -46,4 +52,19 @@ public class PaymentController {
             return new CommonResult(444, "没有对应记录,查询ID: " + id, null);
         }
     }
+
+    @GetMapping(value = "/payment/discovery")
+    public Object discovery() {
+        List<String> services = discoveryClient.getServices();
+        for (String element : services) {
+            log.info("*****element: " + element);
+        }
+
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        for (ServiceInstance instance : instances) {
+            log.info(instance.getServiceId() + "\t" + instance.getHost() + "\t" + instance.getPort() + "\t" + instance.getUri());
+        }
+        return this.discoveryClient;
+    }
+
 }
