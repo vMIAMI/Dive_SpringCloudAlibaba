96a64159c36ba3c88ad5e5a9ec7c68eace110bc2
vMIAMI <1411079322@qq.com>
2020-07-04 21:01:23 +0800

    hystrix

---
+++ cloud-consumer-hystrix-order80/src/main/java/com/vime/OrderHystirxMain80.java
@@ -0,0 +1,17 @@
+package com.vime;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+
+@SpringBootApplication
+@EnableFeignClients
+@EnableHystrix
+public class OrderHystirxMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderHystirxMain80.class, args);
+    }
+}
