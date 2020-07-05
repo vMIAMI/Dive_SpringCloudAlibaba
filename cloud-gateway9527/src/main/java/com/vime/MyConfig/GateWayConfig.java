abfdaf3009cd7c460790d619eca7333546c1e438
vMIAMI <1411079322@qq.com>
2020-07-05 18:44:10 +0800

    gateway

---
+++ cloud-gateway9527/src/main/java/com/vime/MyConfig/GateWayConfig.java
@@ -0,0 +1,27 @@
+package com.vime.MyConfig;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @auther zzyy
+ * @create 2020-02-21 11:42
+ */
+@Configuration
+/**
+ * 自定义gateway 路径转发配置
+ */
+public class GateWayConfig {
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder routeLocatorBuilder) {
+        RouteLocatorBuilder.Builder routes = routeLocatorBuilder.routes();
+
+        routes.route("path_route_atguigu",
+                r -> r.path("/guonei")
+                        .uri("http://news.baidu.com/guonei")).build();
+
+        return routes.build();
+    }
+}
