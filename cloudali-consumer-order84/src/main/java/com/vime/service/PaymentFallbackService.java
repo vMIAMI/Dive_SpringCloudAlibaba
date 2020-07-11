b693ea308a7a838c00bc5a3734e06b07dc7f96c6
vMIAMI <1411079322@qq.com>
2020-07-11 16:20:51 +0800

    sentinel

---
+++ cloudali-consumer-order84/src/main/java/com/vime/service/PaymentFallbackService.java
@@ -0,0 +1,17 @@
+package com.vime.service;
+
+import com.vime.entites.CommonResult;
+import com.vime.entites.Payment;
+import org.springframework.stereotype.Component;
+
+/**
+ * @auther zzyy
+ * @create 2020-02-25 18:30
+ */
+@Component
+public class PaymentFallbackService implements PaymentService {
+    @Override
+    public CommonResult<Payment> paymentSQL(Long id) {
+        return new CommonResult<>(44444, "服务降级返回,---PaymentFallbackService", new Payment(id, "errorSerial"));
+    }
+}
---
+++ cloudali-consumer-order84/src/main/java/com/vime/service/PaymentService.java
@@ -0,0 +1,17 @@
+package com.vime.service;
+
+import com.vime.entites.CommonResult;
+import com.vime.entites.Payment;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * @auther zzyy
+ * @create 2020-02-25 18:15
+ */
+@FeignClient(value = "nacos-payment-provider", fallback = PaymentFallbackService.class)
+public interface PaymentService {
+    @GetMapping(value = "/paymentSQL/{id}")
+    public CommonResult<Payment> paymentSQL(@PathVariable("id") Long id);
+}
